338003ba508fcaa2037dd19bf60d0ea188116316
Mrunali Deore <107094589+MrunaliDeore@users.noreply.github.com>
2022-09-06 17:13:59 +0530

    Update UserController.java

--- UserController.java
+++ UserController.java
@@ -9,10 +9,7 @@ import com.example.bookstoreapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -28,5 +25,33 @@ public class UserController {
     public ResponseEntity<ResponseDTO> createUser(@Valid @RequestBody UserDTO userDTO) {
         ResponseDTO responseDTO = new ResponseDTO("New user add into DB Successfully", userService.addData(userDTO));
         return new ResponseEntity<>(responseDTO, HttpStatus.CREATED);
+    }
+
+    //list of all in the DB - post method
+    @PostMapping("/post")
+    public ResponseEntity<ResponseDTO> listofAll (){
+        ResponseDTO responseDTO = new ResponseDTO("List of All Data in the Database", userService.listAll());
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+    }
+
+    //get data by id from table - get method
+    @GetMapping("/get/{id}")
+    public ResponseEntity<ResponseDTO> getById (@PathVariable int userid){
+        ResponseDTO responseDTO = new ResponseDTO("Get data by ID:", userService.getById(userid));
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+    }
+
+    //delete by id - delete method
+    @DeleteMapping ("/delete/{id}")
+    public ResponseEntity<ResponseDTO> deleteUser(@PathVariable int userid) {
+        ResponseDTO responseDTO = new ResponseDTO("Book Deleted Successfully", userService.deleteById(userid));
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+    }
+
+    //update by id - put method using
+    @PutMapping("update/{id}")
+    public ResponseEntity<ResponseDTO> editUser(@Valid @RequestBody UserData userData, @PathVariable int userid) {
+        ResponseDTO responseDTO = new ResponseDTO("Book Updated Successfully", userService.editById(userData,userid));
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
     }
 }
